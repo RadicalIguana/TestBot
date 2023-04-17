97ac59ff3e7c9576d55f6314df6578c702420401
Kirilll1 <kiryamishatkin@icloud.com>
2023-04-17 15:24:34 +0300

    update

--- testbot.py
+++ testbot.py
@@ -3,10 +3,46 @@ import asyncio
 import logging
 from aiogram import Dispatcher, types, executor
 from token_bot import bot
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 #from aiogram.fsm.context import FSMContext
 
 logging.basicConfig(level=logging.INFO)
-dp=Dispatcher(bot)
+memstore=MemoryStorage()
+dp=Dispatcher(bot, storage=memstore)
+
+class Form(StatesGroup):
+	first_name=State()
+	last_name=State()
+	phone_name=State()
+
+@dp.message_handler(text=['Создать пользователя'], state=None)
+async def asd(message: types.Message):
+	await bot.send_message(message.chat.id, "Введите ваше имя")
+	await Form.first_name.set()
+	
+@dp.message_handler(state=Form.first_name)
+async def set_fname(message: types.Message, state:FSMContext):
+	async with state.proxy() as proxy:
+		proxy['first_name']= message.text
+		await Form.last_name.set()
+	await bot.send_message(message.chat.id, "Введите вашу фамилию")
+
+@dp.message_handler(state=Form.last_name)
+async def set_lname(message:types.Message, state:FSMContext):
+	async with state.proxy() as proxy:
+		proxy['last_name']=message.text
+		await Form.phone_name.set()
+	await bot.send_message(message.chat.id,"Введите ваш номер телефона")
+	
+@dp.message_handler(state=Form.phone_name)
+async def phone_name(message:types.Message, state:FSMContext):
+	async with state.proxy() as proxy:
+		proxy['phone_name']=message.text
+		await state.finish()
+	s=await state.get_data()
+	await bot.send_message(message.chat.id,"Пользователь успешно создан!\n"f'Ваши данные: {s.get("first_name")} {s.get("last_name")} {s.get("phone_name")}')
 
 @dp.message_handler(commands=['start'])
 async def start(message:types.Message):
@@ -19,12 +55,12 @@ async def start(message:types.Message):
 
 @dp.message_handler(text=['Создать пользователя'])
 async def create_user(message:types.Message):
-	await bot.send_message(message.from_user.id, f"Вы {message.from_user.full_name}?")
+	#await bot.send_message(message.from_user.id, f"Вы {message.from_user.full_name}?")
+	await bot.send_message(message.chat.id,"Введите имя")
 	
-	#await bot.send_message(message.chat.id,"Введите имя, фамилию")
-
    # await state.set_state(NameChoosing.choosing_name_lastname)
-	
+
+
 	
     
 
